c1ea62f59ca089c5410ebf6969aa1cab3e595ab2
unknown <sonleuid2@gmail.com>
2025-11-21 19:47:36 +0700

    haha'

---
+++ Lab01/Ass63/src/ass63/StarTriangle.java
@@ -0,0 +1,28 @@
+package ass63;
+
+import java.util.Scanner;
+
+public class StarTriangle {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter the height of the triangle (n): ");
+        int n = input.nextInt();
+
+        for (int i = 1; i <= n; i++) {
+            // In khoảng trắng trước
+            for (int j = i; j < n; j++) {
+                System.out.print(" ");
+            }
+            // In dấu *
+            for (int k = 1; k <= (2 * i - 1); k++) {
+                System.out.print("*");
+            }
+            System.out.println(); // Xuống dòng
+        }
+
+        input.close();
+    }
+}
+
+
